caad9dd110d5862d5e43af39586b82f2b2102a3b
Nimbus1997 <blue8smile@naver.com>
2022-07-14 16:32:44 +0900

    outdoor data preprocessing_will use whold size

--- data_transform/outdoor_rotate90.py
+++ data_transform/outdoor_rotate90.py
@@ -1,21 +1,20 @@
 # -----------------------------
-# (0) image 90 도 돌아간 것 돌리고, crop 시작
-# (1) image crop & (2)flip & _00(f).jpg 로 생성됨.
-# test * val : 한 이미지 당 12개 생성
-# train : 한 이미지당 *45(crop) *2 (flip)
+# image rotate 90
 # -----------------------------
 # for nonhomogeneous outdoor data(1200 x 1600 size) from CVPRw
+# outdoor data중 몇개가 90도 돌아가 있어서 
 # data from: https://competitions.codalab.org/competitions/22236#participate
 #          : https://competitions.codalab.org/competitions/28032#participate
-# reference: https://webnautes.tistory.com/1653
+# reference: https://pythonexamples.org/python-pillow-rotate-image-90-180-270-degrees/ 
 #
 # made by: Ellen jieun oh 
-# date: 2022.07.10
+# date: 2022.07.14
 # email: jieunoh@postech.ac.kr
-# modified: 
+# modified: 2022.07.14
 
 
 import os
+from PIL import Image
 import cv2 as cv
 import numpy as np
 
@@ -23,7 +22,12 @@ ori_path = "/home/guest1/ellen_data/outdoor_rename"
 gen_path = "/home/guest1/ellen_data/outdoor_rename_512"
 
 folders = ['trainA','trainB', 'valA', 'valB', 'testA','testB']
+rotated_list = [[1,2,5,6,11,21],[12,17,18], [],[1,2,5,6,11], [4,8,9,10,12],[4,8,9,10,12]] #돌아간 이름 폴더 순으로 
 
+# ---------------------------------
+# 1. setting 
+# ---------------------------------
+# [Def]CROP: center 값 주어면, size *size로 crop ---------------------------------
 def center_crop(img:np.array,x:int,y:int, size:int) -> np.array:
     ss = size//2
     # size == 512
@@ -32,16 +36,16 @@ def center_crop(img:np.array,x:int,y:int, size:int) -> np.array:
 
 # crop 어떻게 할지 center point - x, y ---------------------------------
 size = 512
-x_width=1200
-y_width= 1600
+height = 1200
+width = 1600
 
-tvx=[256, 344 ,688]
-# x=[size//2, (size//2+(1200-size//2))//2 ,1200-size//2]
-tvy=[256,618,982,1344]
-# y=[size//2, 사이//3 해서 등간격 ,1600-size//2]
+# test& val는 조금만 자름
+tvx=[256, 344 ,688]# x=[size//2, (size//2+(1200-size//2))//2 ,1200-size//2]
+tvy=[256,618,982,1344]# y=[size//2, 사이//3 해서 등간격 ,1600-size//2]
+# train은 자잘하게 자름 
+x=list(range(256,688,100)) #  5개
+y=list(range(256,1344,150)) # 8개 
 
-x=list(range(256,688,150))
-y=list(range(256,1344,150))
 
 # folder가 없으면, 생성 ---------------------------------
 if not os.path.isdir(gen_path):
@@ -51,16 +55,31 @@ if not os.path.isdir(gen_path):
 print("Path generated END----------------------------------------------------------------")
 
 
-# 이미지 crop  & 저장 --------------------------------- 
-for f in folders:
+# ---------------------------------
+# 2. image rotate, crop, flip
+# ---------------------------------
+# [1] 폴더별로 열기-----------------
+for ii,f in enumerate(folders):
+    rotation_check = rotated_list[ii]
+    # [2] 폴더별로 열기-----------------
     if "train" in f:
-        for img in sorted(os.listdir(ori_path+"/"+f)):
+        for ith, img in enumerate(sorted(os.listdir(ori_path+"/"+f))):
             imgg = cv.imread(ori_path+"/"+f+"/"+img)
+            roated= False
             i = 0
+
+            # # image가 돌아가 있는 경우는 사진을 돌리고, 범위도 바꿔줘야함
+            # if ith in rotation_check: 
+            #     temp = x
+            #     x=y
+            #     y=temp
+                
+            
             for xx in x:
                 for yy in y:
+                    
                     imgg_croped = center_crop(imgg, xx,yy, size)
-                    cv.imwrite(gen_path+"/"+f+"/"+img.split(".")[0]+"_"+str(i).zfill(2)+".jpg", imgg_croped)
+                    cv.imwrite(gen_path+"/"+f+"/"+img.split(".")[0]+"_"+str(i).zfill(2)+".png", imgg_croped)
 
                     i+=1
         print(f+" croped & filped----")
@@ -72,7 +91,7 @@ for f in folders:
             for xx in tvx:
                 for yy in tvy:
                     imgg_croped = center_crop(imgg, xx,yy, size)
-                    cv.imwrite(gen_path+"/"+f+"/"+img.split(".")[0]+"_"+str(i).zfill(2)+".jpg", imgg_croped)
+                    cv.imwrite(gen_path+"/"+f+"/"+img.split(".")[0]+"_"+str(i).zfill(2)+".png", imgg_croped)
                     i+=1
         print(f+" croped----")
 print("Image cropping END----------------------------------------------------------------")
